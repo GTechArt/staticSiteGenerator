c5a998846bd6167f1d09bdae379a5cc7f98f9953
choub <guillaume.choubard@gmail.com>
2026-04-10 20:50:59 +0200

    add split nodes with delimeter + unit tests

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from textnode import TextNode, TextType, text_node_to_html_node
+from textnode import TextNode, TextType, text_node_to_html_node, split_nodes_delimiter
 
 
 class TestTextNode(unittest.TestCase):
@@ -60,6 +60,53 @@ class TestTextNode(unittest.TestCase):
         html_node = text_node_to_html_node(node)
         self.assertEqual(html_node.tag, "img")
         self.assertEqual(html_node.value, "")
+
+    def test_split_nodes_delimiter_code(self):
+        node = TextNode("This is text with a `code block` word", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
+        self.assertEqual(new_nodes, [
+            TextNode("This is text with a ", TextType.TEXT),
+            TextNode("code block", TextType.CODE),
+            TextNode(" word", TextType.TEXT),
+            ])
+        
+    def test_split_nodes_delimiter_bold(self):
+        node = TextNode("This is text with a **bold block** word", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node], "**", TextType.BOLD)
+        self.assertEqual(new_nodes, [
+            TextNode("This is text with a ", TextType.TEXT),
+            TextNode("bold block", TextType.BOLD),
+            TextNode(" word", TextType.TEXT),
+            ])
+        
+    def test_split_nodes_delimiter_italic(self):
+        node = TextNode("This is text with a _italic block_ word", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node], "_", TextType.ITALIC)
+        self.assertEqual(new_nodes, [
+            TextNode("This is text with a ", TextType.TEXT),
+            TextNode("italic block", TextType.ITALIC),
+            TextNode(" word", TextType.TEXT),
+            ])
+        
+    def test_split_nodes_delimiter_bold_x3(self):
+        node = TextNode("This **is** text **with** a **bold block** word", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node], "**", TextType.BOLD)
+        self.assertEqual(new_nodes, [
+            TextNode("This ", TextType.TEXT),
+            TextNode("is", TextType.BOLD),
+            TextNode(" text ", TextType.TEXT),
+            TextNode("with", TextType.BOLD),            
+            TextNode(" a ", TextType.TEXT),
+            TextNode("bold block", TextType.BOLD),
+            TextNode(" word", TextType.TEXT),
+            ])
+        
+    def test_split_nodes_delimiter_code_error_delimitter(self):
+        with self.assertRaises(Exception) as context:
+            node = TextNode("This is text with a `code block` word`", TextType.TEXT)
+            new_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
+            self.assertEqual(str(context.exception), "Closing delimiter : '`' is missing !")
+
 
 if __name__ == "__main__":
     unittest.main()
--- src/textnode.py
+++ src/textnode.py
@@ -48,3 +48,22 @@ def text_node_to_html_node(text_node):
         case _:
             raise Exception ("text_type is missing or not recognizing")
             
+def split_nodes_delimiter(old_nodes, delimiter, text_type):
+    new_nodes = []
+    for old_node in old_nodes:
+        split_texts = old_node.text.split(delimiter)
+
+        if len(split_texts) % 2 == 0:
+            raise Exception(f"Closing delimiter : '{delimiter}' is missing !")
+
+        for i in range(len(split_texts)):
+            if i % 2 == 0:
+                new_nodes.append(TextNode(split_texts[i], TextType.TEXT))
+            else:
+                new_nodes.append(TextNode(split_texts[i], text_type))
+
+    return new_nodes
+        
+
+
+    
